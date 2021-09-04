a3f8a2e32e128e040bf6d4d4cec14dfbcb0f1c56
Ramon Barbosa <ramonmfb777@gmail.com>
2021-09-04 12:20:26 -0300

    fixing error

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -13,6 +13,8 @@ import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
 import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.entities.User;
 
 @Service
 public class EventService {
@@ -23,12 +25,16 @@ public class EventService {
 	
 	@Transactional
 	public EventDTO update(Long id, EventDTO dto) {
-			Optional<Event> obj = repository.findById(id);
-			Event entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found" + id));
+		try {
+			Event entity = repository.getOne(id);
 	        copyDtoToEntity(dto, entity);
 	        entity = repository.save(entity);
 	        return new EventDTO(entity);
 		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
 	
 	private void copyDtoToEntity(EventDTO dto, Event entity) {
 		entity.setName(dto.getName());
